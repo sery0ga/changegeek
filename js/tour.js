e48058fe3ade1292ca465b611e757f30a14404b9
Sergey Kotlov <skotlov@gmail.com>
2015-04-05 17:14:33 +0300

    Adds new menu to Tour page
    It was not very clear that companies and people were switching
    together with cities

--- js/tour.js
+++ js/tour.js
@@ -2,13 +2,25 @@ function hideCityData() {
   $('.meetings').hide();
   $('.credits').children('div').hide();
 }
+function resetCityMenu() {
+  $('#cityMenu').children('li').children('a').removeClass('active');
+}
 
 $(document).ready(function() {
   hideCityData();
+  resetCityMenu();
   $('.helsinki').show();
+  $('a.helsinki').addClass('active');
   $('#cities').on('slide.bs.carousel', function (e) {
     hideCityData();
+    resetCityMenu();
     var className = $(e.relatedTarget).attr('id');
     $('.' + className).show();
+    $('a.' + className).addClass('active');
+  });
+  $('#cityMenu').children('li').children('a').on('click', function(e) {
+    e.preventDefault();
+    var index = $(this).data('id');
+    $('.carousel').carousel(index);
   });
 });
